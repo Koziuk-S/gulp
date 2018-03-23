fb1943570747209a24ceac9df0e510d541e667bb
serhiy.koziuk <serhiy.koziuk@eleks.com>
2018-03-23 15:10:19 +0200

    first commit

---
+++ gulp-starter/README.md
@@ -0,0 +1,31 @@
+# Test-3
+### Version
+1.0.0
+# Getting Started
+
+#### 1. Install npm:
+
+[node.js](https://nodejs.org/en/)
+
+#### 2. Install Gulp globally:
+
+__If you have previously installed a version of gulp globally, please run `npm rm --global gulp`
+to make sure your old version doesn't collide with gulp-cli.__
+
+```sh
+$ npm install --global gulp-cli
+```
+#### 3. If you have a package.json file and there is devDependencies which we need:
+```sh
+npm install -d
+```
+#### 4. Use command to build:
+
+```sh
+$ gulp build
+```
+#### 4. Use command to run:
+
+```sh
+$ gulp
+```
---
+++ gulp-starter/gulpfile.js
@@ -0,0 +1,122 @@
+'use strict';
+
+const gulp = require('gulp'),
+
+  del = require('del'),
+  notify = require('gulp-notify'),
+  rigger = require('gulp-rigger'),
+  rename = require('gulp-rename'),
+  sourcemaps = require('gulp-sourcemaps'),
+
+  imagemin = require('gulp-imagemin'),
+  pngquant = require('imagemin-pngquant'),
+
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  minifyCSS = require('gulp-minify-css'),
+
+  babel = require('gulp-babel'),
+  uglyfly = require('gulp-uglyfly'),
+
+  connect = require('gulp-connect'),
+  livereload = require('gulp-livereload');
+
+const paths = {
+  src: __dirname + '/src/',
+  dist: __dirname + '/build/'
+};
+
+gulp.task('clean', function () {
+  return del(paths.dist + '**', { force: true });
+});
+
+// server connect
+gulp.task('connect', function () {
+  connect.server({
+    port: 5005,
+    root: paths.dist,
+    livereload: true
+  });
+});
+
+// html
+gulp.task('html', function () {
+  gulp.src(paths.src + 'index.html')
+    .pipe(rigger())
+    .pipe(gulp.dest(paths.dist))
+    .pipe(connect.reload())
+    .pipe(notify({
+      message: 'HTML task is done.',
+      onLast: true
+    }));
+});
+
+// styles
+gulp.task('styles', function () {
+  gulp.src(paths.src + 'styles/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(minifyCSS())
+    .pipe(rename('app.min.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(connect.reload())
+    .pipe(notify({
+      message: 'Styles are compiled.',
+      onLast: true
+    }));
+});
+
+// js
+gulp.task('js', function () {
+  gulp.src(paths.src + 'js/main.js')
+    .pipe(rigger())
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglyfly())
+    .pipe(rename('app.min.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(connect.reload())
+    .pipe(notify({
+      message: 'JS is compiled.',
+      onLast: true
+    }));
+});
+
+// img
+gulp.task('img', function () {
+  gulp.src(paths.src + 'img/**/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      use: [pngquant()],
+      interlaced: true
+    }))
+    .pipe(gulp.dest(paths.dist + 'img/'))
+    .pipe(connect.reload())
+    .pipe(notify({
+      message: 'IMG task is done.',
+      onLast: true
+    }));
+});
+
+// watch
+gulp.task('watch', function () {
+  gulp.watch('src/html/**/*.html', ['html'])
+  gulp.watch('src/scss/**/*.scss', ['styles'])
+  gulp.watch('src/js/**/*.js', ['js'])
+  gulp.watch('src/img/**/*', ['img'])
+});
+
+// default
+gulp.task('default', ['connect', 'html', 'styles', 'js', 'watch']);
+
+// build
+gulp.task('build', ['clean', 'html', 'styles', 'js', 'img']);
