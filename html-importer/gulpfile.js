ca3e1f6f90dabea6073cc69329587cd8069abd4b
serhiy.koziuk <serhiy.koziuk@eleks.com>
2018-04-04 18:25:58 +0300

    added html beautify and fix code

--- html-importer/gulpfile.js
+++ html-importer/gulpfile.js
@@ -1,30 +1,42 @@
+// NODE modules
 const gulp = require('gulp'),
-    importer = require('./importer'),
-    templates = require('./templates-ads'),
-    del = require('del');
+  del = require('del'),
+  htmlBeautify = require('gulp-html-beautify');
+
+// My modules
+const importer = require('./importer'),
+  templates = require('./templates-ads');
 
 const paths = {
-    src: __dirname + '/src/',
-    dist: __dirname + '/build/'
+  src: __dirname + '/src/',
+  dist: __dirname + '/build/'
 };
 
+// gulp task CLEAN
 gulp.task('clean', function () {
-    return del(paths.dist + '**', { force: true });
+  return del(paths.dist + '**', { force: true });
 });
 
+// gulp task MARCUP (compile html)
 gulp.task('markup', function () {
-    return gulp.src(paths.src + 'emails/*.html')
-        .pipe(importer({
-            src: paths.src,
-            templates: templates
-        }))
-        .pipe(gulp.dest(paths.dist));
+  return gulp.src(paths.src + 'emails/*.html')
+    .pipe(importer({
+      src: paths.src,
+      templates: templates
+    }))
+    .pipe(htmlBeautify({
+      html: { indent_size: 2 }
+    }))
+    .pipe(gulp.dest(paths.dist));
 });
 
-// watch
+// gulp task WATCH
 gulp.task('watch', function () {
-    gulp.watch(paths.src + '**/*.html', gulp.series('markup'));
+  gulp.watch(paths.src + '**/*.html', gulp.series('markup'));
 })
 
-// // default
+// gulp task DEFAULT
 gulp.task('default', gulp.series('clean', 'markup', 'watch'));
+
+// gulp task BUILD
+gulp.task('build', gulp.series('clean', 'markup'));
